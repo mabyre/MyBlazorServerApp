76eb238a14ce4f698a12749ee34c34f24d8a2819
mabyre <mabyre@sodevlog.com>
2023-02-21 14:48:36 +0100

    there was a space caract missing

--- Data/WeatherForecastService.cs
+++ Data/WeatherForecastService.cs
@@ -4,8 +4,8 @@ namespace MyBlazorServerApp.Data
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
         {
