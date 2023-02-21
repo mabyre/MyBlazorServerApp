f191e737bd1ddb4d41ef2312f19e2a0e3ab78092
mabyre <mabyre@sodevlog.com>
2023-02-21 11:30:10 +0100

    Add Page and Nav for ToDo List

---
+++ Pages/TodoList.razor.cs
@@ -0,0 +1,21 @@
+﻿using BlazorApp1.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace MyBlazorServerApp.Pages
+{
+    public class TodoListBase : ComponentBase
+    {
+        protected List<TodoItem> todos = new();
+        protected string? newTodo;
+
+        protected void AddTodo()
+        {
+            // Add the todo
+            if (!string.IsNullOrWhiteSpace(newTodo))
+            {
+                todos.Add(new TodoItem { Title = newTodo });
+                newTodo = string.Empty;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-﻿### Separate Code and View
+﻿### Add a TodoList
+Sample ToDoList in Balzor
+
+### Separate Code and View
 
 * Look in Pages/Counter seperate by derivate from ComponentBase
