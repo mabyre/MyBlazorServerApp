f87b42fadd13984a5a989dd142cc579f72828d5f
mabyre <mabyre@sodevlog.com>
2023-02-27 16:00:28 +0100

    NavigationManager

---
+++ Pages/Navigate.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.Extensions.Logging;
+
+namespace MyBlazorServerApp.Pages;
+
+public partial class Navigate : ComponentBase, IDisposable
+{
+    [Inject]
+    NavigationManager Navigation { get; set; }
+
+    [Inject]
+    ILogger<Navigate> Logger { get; set; }
+
+    private void NavigateToCounterComponent()
+    {
+        Navigation.NavigateTo("counter");
+    }
+
+    protected override void OnInitialized()
+    {
+        Navigation.LocationChanged += HandleLocationChanged;
+    }
+
+    private void HandleLocationChanged(object? sender, LocationChangedEventArgs e)
+    {
+        Logger.LogInformation("URL of new location: {Location}", e.Location);
+    }
+
+    public void Dispose()
+    {
+        Navigation.LocationChanged -= HandleLocationChanged;
+    }
+}
