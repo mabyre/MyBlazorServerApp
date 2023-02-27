90bb17a924eeeac4eb1ad602a6fd0d30a938c103
mabyre <mabyre@sodevlog.com>
2023-02-27 16:41:52 +0100

    ManyRout NavigationManager PreventNavigation

---
+++ Pages/NavHandler.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace MyBlazorServerApp.Pages;
+
+public partial class NavHandler : ComponentBase, IDisposable
+{
+    [Inject]
+    NavigationManager Navigation { get; set; }
+
+    private IDisposable? registration;
+
+    private bool isPrevent = false;
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender)
+        {
+            registration =
+                Navigation.RegisterLocationChangingHandler(OnLocationChanging);
+        }
+    }
+
+    private ValueTask OnLocationChanging(LocationChangingContext context)
+    {
+        if (context.TargetLocation == "/counter")
+        {
+            context.PreventNavigation();
+            isPrevent = true;
+        }
+
+        return ValueTask.CompletedTask;
+    }
+
+    public void Dispose() => registration?.Dispose();
+
+}
