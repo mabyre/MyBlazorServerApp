bab6bb94de8082e59235826177bd8d573dbf0172
mabyre <mabyre@sodevlog.com>
2023-03-06 11:38:09 +0100

    Page CounterSave to use ProtectedBrowserStorage

---
+++ Pages/CounterSave.razor.cs
@@ -0,0 +1,30 @@
+﻿//
+// User Data (counter's value) are saved form reloading page
+//
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace MyBlazorServerApp.Pages;
+
+public class CounterSaveBase : ComponentBase
+{
+    protected int currentCount = 0;
+
+    private int valueCounter = 2;
+
+    [Inject]
+    private ProtectedSessionStorage ProtectedSessionStore { get; set; }
+
+
+    protected async Task ButtonCounterOnClickAsync()
+    {
+        currentCount += valueCounter;
+        await ProtectedSessionStore.SetAsync("count", currentCount);
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var result = await ProtectedSessionStore.GetAsync<int>("count");
+        currentCount = result.Success ? result.Value : 0;
+    }
+}
